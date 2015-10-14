92afe8712a5259514fdf7bec69462ac36d50d786
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-14 17:01:55 -0600

    added personas and began website

--- personasandusecases.php
+++ personasandusecases.php
@@ -1,25 +1,139 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Personas and Use Cases</title>
+		<head>
+			<meta charset="utf-8"/>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1"/>
+			<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+			<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+			<link type="text/css" href="css/styles.css" rel="stylesheet"/>
+			<link type="text/css" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css"/>
+			<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+			<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+			<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js"></script>
+			<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/additional-methods.min.js"></script>
+			<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+			<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+			<script src= "js/script.js"></script>
+			<title>Personas and Use Cases</title>
 	</head>
 	<body>
-		<h1>Standard Seller Profile<>
+		<main>
+		<div>
+			<h1>Personas</h1>
+		</div>
+			<h2>Standard Seller Profile</h2>
 		<ul>
 			<li> age: 18-65 </li>
-			<li> homemaker, student, independent fashion designer, small company</li>
+			<li> career </li>
+					<li>homemaker</li>
+					<li>student</li>
+					<li>fashion designer</li>
+					<li>small fashion company</li>
+					<li>overseas company</li>
+			</ul>
+		<h3>Seller Process</h3>
+			<ol>
+				<li>Click on "Sell on Etsy"</li>
+				<li>Link doesn't work</li>
+				<li> Click on "Open your Etsy Shop</li>
+				<li>Link takes you to Sign in page, which seller cannot do
+				as he is not yet a member</li>
+				<li> Click on Register</li>
+				<li> Complete Information and click "Register" button</li>
+				<li>Click "yes" button to choose US dollar as currency or choose a
+				different currency</li>
+				<li>message appears to check email.  Go to your email to confirm account</li>
+				<li>if you typed wrong email, click button to change email address.  Message appears
+				to close account. Another message appears that account is uncomfirmed.  X out of these. </li>
+				<li>message appears that you didn't click to accept change to your account in the wrong
+				email address and offers to resend confirmation.  Even though you cannot access that email address</li>
+				<li> press back arrow or reenter website in search tool bar to start the process all over again</li>
+				<li> A message shows that you are signed in.  Apparantly you DON'T have to confirm email</li>
+				<li>Choose what you hope to accomplish from provided options on popup window</li>
+				<li>You are directed to fill out shop name in provided box</li>
+				<li>click "save"</li>
+				<li>The style guide here is very simple to follow.  Click the plus sign to add a listing</li>
+				<li>popup window says a message was sent to you adjusted email.  You can now check and confirm this</li>
+				<li>confirmation link in email brings you to a new page and a popup window says
+				that your account was confirmed.  However, you are no longer on the page you were working on</li>
+				<li>click "your shop"</li>
+				<li>click the plus sign to add a listing</li>
+				<li>follow the simple directions to add photos and listing details</li>
+				<li> click to preview listing</li>
+				<li> edit or save and continue</li>
+				<li> add more listings and or continue</li>
+				<li>Set up Direct Checkout</li>
+			</ol>
 		</h1>
 		<h2>Seller 1</h2>
 		<ul>
 			<li>Name:Rebecca Herrera</li>
+			<li>Goal:Earn as much money as possible, gain a reputation, and open a big store</li>
 			<li>Age: 47</li>
 			<li>Career: Fashion Student</li>
 			<li>Product:High quality one-of-a-kind clothing</li>
-			<li>Product Prices: 160-240 </li>
+			<li>Product Prices: $160-240 </li>
 		</ul>
 		<h2>Seller 2</h2>
+
 		<ul>
 			<li>Name:John Edwards</li>
+			<li>Goal: Enjoys making products and want to ease life and make people happy</li>
+			<li>Age:23</li>
+			<li>Career:Comcast Installer</li>
+			<li>Product: Inexpensive wooden medicine containers</li>
+			<li>Product Prices: 4-8 </li>
 		</ul>
+			<h1>Standard Buyer Profile</h1>
+				<ul>
+					<li>age:18-65</li>
+					<li>personality: individualistic</li>
+					<li>various</li>
+				</ul>
+			<h2>Seller 3</h2>
+					<ul>
+						<li> Name: Jonathon Smith</li>
+						<li> Goal: hoping to get his name out and become famous</li>
+						<li> age:35</li>
+						<li> Career: computer technician</li>
+						<li> product: paintings</li>
+						<li> Product Prices: 75</li>
+					</ul>
+			<h2>Seller 4</h2>
+				<ul>
+					<li>Name:Rebecca Faris</li>
+					<li>Goal:Earn a lot of money</li>
+					<li>age:29</li>
+					<li>Career:Owner of SW Fashion, the products of which she sells on Etsy</li>
+					<li>product:clothing</li>
+					<li>Product Prices: 50-135</li>
+				</ul>
+			<h2>Buyer 1</h2>
+				<ul>
+					<li>Name: KC Miera</li>
+					<li>Goal: Just browsing.  Not looking for anything special and doesn't want to spend a lot of money</li>
+					<li>Price Range: up to $20/li>
+					<li>age</li>
+					<li>career: waitress</li>
+					<li>interests: jewelry</li>
+					<li>personality- individual, hippy</li>
+					<li>reaction: Does not like that you have to register to login.  Takes awhile before
+					taking the time to do so.  Finds a multitude of inexpensive, unique items.  Favorites
+					designers and, after initial purchase, becomes a regular customer.</li>
+				</ul>
+			<h2>Buyer 2<h2>
+				<ul>
+					<li>Name:Samantha Ponker</li>
+					<li>Goal: Wants to be a custom halloween costume</li>
+					<li>Price Range : up to 400 </li>
+					<li>Career:Student</li>
+						<p>Reaction.  Difficulty finding high quality items on etsy.</p>
+			<h2> Buyer 3<h2>
+
+
+		</main>
 	</body>
 </html>
