06f0a0a19ce44a89890768bb225983ae21dd805e
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-14 15:58:56 -0600

    added personas and began website

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>etsy product and seller</title>
-	</head>
-		<p>Etsy</p>
+<?php
+require_once 'header.php';
+
+echo "<br><span class='main'>Welcome to $appname,";
+
+if ($loggedin) echo " $user, you are logged in.";
+else           echo ' please sign up and/or log in to join in.';
+?>
+
+</span><br><br>
+</body>
 </html>
---
+++ personasandusecases.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Personas and Use Cases</title>
+	</head>
+	<body>
+		<h1>Standard Seller Profile<>
+		<ul>
+			<li> age: 18-65 </li>
+			<li> homemaker, student, independent fashion designer, small company</li>
+		</h1>
+		<h2>Seller 1</h2>
+		<ul>
+			<li>Name:Rebecca Herrera</li>
+			<li>Age: 47</li>
+			<li>Career: Fashion Student</li>
+			<li>Product:High quality one-of-a-kind clothing</li>
+			<li>Product Prices: 160-240 </li>
+		</ul>
+		<h2>Seller 2</h2>
+		<ul>
+			<li>Name:John Edwards</li>
+		</ul>
+	</body>
+</html>
